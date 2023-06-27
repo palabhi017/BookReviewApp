8661ba9b03cc962f78d0666c93caf116f65cf9e7
palabhi017 <palabhi017@gmail.com>
2023-06-27 14:20:26 +0530

    loader added

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,16 +1,20 @@
-import { Grid } from '@chakra-ui/react'
+import { Grid, Spinner } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import BookCard from '../Components/BookCard'
 import axios from 'axios'
 
 const Homepage = () => {
   const [allBooks,setAllBooks] = useState([])
+  const [load,setLoad] = useState(false)
   const getAllBooks= async()=>{
+  setLoad(true)
       try {
         let res = await axios.get(`https://gifted-fox-sneakers.cyclic.app/book/`)
          setAllBooks(res.data)
+         setLoad(false)
       } catch (error) {
         console.log(error)
+        setLoad(false)
       }
   }
 
@@ -18,9 +22,12 @@ const Homepage = () => {
     getAllBooks()
   },[])
   return (
+    <>
+    {load? <Spinner mt="30px" size={"xl"}/>:""}
     <Grid templateColumns={"repeat(4,200px)"} w="60vw">
        {allBooks.length>0 && allBooks.map((e)=> <BookCard key={e._id} data={e}/>)}
     </Grid>
+    </>
   )
 }
 
